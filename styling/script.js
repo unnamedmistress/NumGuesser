ec073681d4cf930fcd0aea704f572ca1c40d84c4
UnnamedMistress <momchrysti@gmail.com>
2022-12-31 14:47:51 -0500

    commit game

--- styling/script.js
+++ styling/script.js
@@ -2,5 +2,19 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+  }  
+const compareGuesses = (_humanGuess, _computerGuess, _targetGuess)=>{
+humanDifference = Math.abs(humanScore - computerScore)
+computerDifference = Math.abs(computerScore - humanScore)
+return humanDifference <= computerDifference}
 
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        computerScore++;
+    } else {
+        humanScore++;
+    }
+}
+const advanceRoundNumber = () => currentRoundNumber++;
